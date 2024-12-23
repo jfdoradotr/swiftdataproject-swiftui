d5f6427f86c3953012c283f5428527c891d11bd6
Juan Dorado <jfdoradotr@icloud.com>
2024-12-23 14:05:33 -0600

    Create the swift data model user

---
+++ SwiftDataProject/SwiftDataProject/User.swift
@@ -0,0 +1,19 @@
+//
+// Copyright © Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class User {
+  var name: String
+  var city: String
+  var joinDate: Date
+
+  init(name: String, city: String, joinDate: Date) {
+    self.name = name
+    self.city = city
+    self.joinDate = joinDate
+  }
+}
