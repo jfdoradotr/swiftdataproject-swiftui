14ae8f7cf79e2ead398712ca14a52f20a3f683d8
Juan Dorado <jfdoradotr@icloud.com>
2024-12-23 14:04:02 -0600

    Initial project added

---
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,14 @@
+//
+// Copyright © Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
